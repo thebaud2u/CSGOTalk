bc5a70fde5155eb5dc96bce823ea63d3213e0d12
thebaud2u <thebaud.clement@gmail.com>
2015-10-28 12:50:49 +0100

    Connection correction, user infos are now in the header

--- src/CSGOTalk/siteBundle/Controller/SiteController.php
+++ src/CSGOTalk/siteBundle/Controller/SiteController.php
@@ -8,56 +8,65 @@ use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 
 class SiteController extends Controller
 {
-    public function indexAction(Request $request)
+    public function menu($request)
     {
-        if($request == null)
+        // Initialisation de la session
+        // 1° cherche si une session existe déjà
+        // 2° génère les infos pour steam : boutton ou pseudo et avatar
+        // 3° valide les infos pour steam
+        $session = $request->getSession();
+        $steamAuth = $this->get('steam_auth');
+        $steamId = $steamAuth->validate();
+
+        // Si le steamId est valide, on l'implémente en session
+        if ($steamId) {
+            $session->set('steamId', $steamId);
+        }
+
+        // Si le steamId n'est pas valide, il n'est pas en session, donc on doit connecter l'utilisateur
+        // On génère donc le boutton de connexion à steam avec redirection vers Home
+        if(!$session->get('steamId'))
         {
             $steamApiKey = '664556FD11D9A256BD39BCBDFE757F60';
-            $steamAuth = $this->get('steam_auth');
             $authUrl = $steamAuth->genUrl('http://localhost:8080/Symfony/web/app_dev.php/home', false);
 
-            return $this->render('CSGOTalksiteBundle:Site:index.html.twig', array(
+            return array(
                 'button' => $authUrl,
                 'nickname' => null,
-                'avatar' => null)
+                'avatar' => null,
+                'userUrl' => null
             );
         }
+        // Le steamId est valide, il existe donc en session
+        // On le récupère pour aller chercher les infos de l'utilisateur grâce à l'API Steam
+        // Dans ces infos, on récupère le pseudo, l'avatar small et l'url de son profil Steam
+        // On créé l'affichage avec ces infos.
         else 
         {
-            $session = $request->getSession();
-
-            $steamAuth = $this->get('steam_auth');
-            $steamId = $steamAuth->validate();
-
-            if ($steamId) {
-                $session->set('steamId', $steamId);
-            }
-
-            // non connecté, redirection sur steam ou la page de connexion (plus tard)
-            if (!$session->get('steamId')) {
-                return $this->redirect($authUrl);
-            }
-            
             $steamId = $session->get('steamId');
             $jsonInfo = file_get_contents("http://api.steampowered.com/ISteamUser/GetPlayerSummaries/v0002/?key=664556FD11D9A256BD39BCBDFE757F60&steamids=$steamId");
             $data = json_decode($jsonInfo, true);
             //var_dump($data['response']['players'][0]);
-            //echo $data['response']['players'][0]['personaname'];
-            //echo '<img src="'.$data['response']['players'][0]['avatar'].'">';
 
-            return $this->render('CSGOTalksiteBundle:Site:index.html.twig', array(
+            return array(
                 'button' => null,
                 'nickname' => $data['response']['players'][0]['personaname'],
-                'avatar' => $data['response']['players'][0]['avatar'])
+                'avatar' => $data['response']['players'][0]['avatar'],
+                'userUrl' => $data['response']['players'][0]['profileurl']
             );
         }
     }
 
-    public function threadsAction()
+    public function indexAction(Request $request)
+    {   
+        $array = self::menu($request);
+        return $this->render('CSGOTalksiteBundle:Site:index.html.twig', $array);
+    }
+
+    public function threadsAction(Request $request)
     {
-    	$threads = $this->getDoctrine()->getManager()->getRepository('CSGOTalksiteBundle:Thread')->getThreads();
-        return $this->render('CSGOTalksiteBundle:Site:threads.html.twig', array(
-        	'threads' => $threads)
-        );
+    	//$threads = $this->getDoctrine()->getManager()->getRepository('CSGOTalksiteBundle:Thread')->getThreads();
+        $array = self::menu($request);
+        return $this->render('CSGOTalksiteBundle:Site:threads.html.twig', $array);
     }
 }
